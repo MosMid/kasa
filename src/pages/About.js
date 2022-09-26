0bb7725f942cfb86e4d75a222f99c16da9b4f525
MosMid <mehoody@yahoo.fr>
2022-09-26 17:30:31 +0200

    5

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@ function About(){
         <Collapsible title='Fiabilité' content='Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.' />
         <Collapsible title='Respect' content='La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'/>
         <Collapsible title='Service' content='Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N&#39;hésitez pas à nous contacter si vous avez la moindre question.'/>
-        <Collapsible title='Sécurité' content='La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l&#39;hôte qu&#39;au locataire cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.'/>
+        <Collapsible title='Sécurité' content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."/>
     </main>
 }
 
